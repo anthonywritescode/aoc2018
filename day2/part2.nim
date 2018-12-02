5fcb82da2d13023a4a465a241c56993e94b90605
Anthony Sottile <asottile@umich.edu>
2018-12-02 15:44:44 -0800

    Implement day 2 part 2 in nim

---
+++ day2/part2.nim
@@ -0,0 +1,18 @@
+import os
+import strutils
+
+let lines = strutils.splitLines(readFile(os.paramStr(1)).strip())
+
+for line in lines:
+    for candidate in lines:
+        var c = 0
+        var lastc = -1
+        for i in 0..len(line):
+            if line[i] != candidate[i]:
+                c += 1
+                lastc = i
+        if c == 1:
+            var copy = "" & line
+            copy.delete(lastc, lastc)
+            echo copy
+            quit(0)
