eb1313be09372058c6738056403cd79448561f3f
Anthony Sottile <asottile@umich.edu>
2018-12-02 13:46:29 -0800

    Add alternative solution for day2 part2

--- day2/part2.py
+++ day2/part2.py
@@ -1,20 +1,54 @@
 import argparse
+from typing import Set
 
 import pytest
 
 
-def compute(s: str) -> str:
+def compute_orig(s: str) -> str:  # O(N^2 * M)
     lines = s.splitlines()
-    for line in lines:
-        for other_line in lines:
+    for line in lines:  # O(N)
+        for other_line in lines:  # O(N)
             assert len(line) == len(other_line), (line, other_line)
             ret = ''
-            for c1, c2 in zip(line, other_line):
+            for c1, c2 in zip(line, other_line):  # O(M)
                 if c1 == c2:
                     ret += c1
             if len(ret) == len(line) - 1:
                 return ret
     raise AssertionError('unreachable!')
+
+
+def _to_substrings(s: str) -> Set[str]:  # O(M) work + O(M) space
+    """bar => {'ar', 'br', 'ba'}"""
+    return {s[:i] + s[i + 1:] for i in range(len(s))}
+
+
+def compute(s: str) -> str:  # O(N * M)
+    seen: Set[str] = set()
+
+    for line in s.splitlines():  # O(N)
+        new_substrings = _to_substrings(line)
+        already_seen = new_substrings & seen  # O(M)
+        if already_seen:
+            answer, = already_seen
+            return answer
+        else:
+            seen.update(new_substrings)  # O(N * M) space
+    else:
+        raise AssertionError('unreachable!')
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        ('', set()),
+        ('a', {''}),
+        ('bar', {'ar', 'br', 'ba'}),
+        ('foo', {'oo', 'fo'}),
+    ),
+)
+def test_to_substrings(input_s: str, expected: Set[str]) -> None:
+    assert _to_substrings(input_s) == expected
 
 
 @pytest.mark.parametrize(
